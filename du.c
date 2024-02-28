f0b1aa48d476023aff2a1717795b29f8ff66e726
Juan Becerra <jdb9056@nyu.edu>
2024-02-27 20:00:51 -0500

    Refactor disk usage calculation for enhanced error handling and modularity
    This commit introduces significant refactoring in the disk usage (du)
    utility's implementation, aimed at enhancing error handling, improving
    the modularity of the code, and ensuring accurate inclusion of the root
    file or directory in the disk usage calculation. Key changes include:
    
    1. Error Handling: The `du` function now returns an integer status code
       instead of `uint64_t`, allowing for more explicit error reporting. This
       change aligns with conventional error handling practices in C, where
       returning negative values indicates errors. The function utilizes an
       error parameter to relay specific error information back to the caller,
       streamlining error handling across recursive calls.
    
    2. Introduction of `dfs` Helper Function: A new helper function, `dfs`,
       has been created to encapsulate the recursive disk usage calculation
       logic. This separation of concerns enhances readability and
       maintainability by isolating the depth-first search algorithm used to
       traverse directories from the higher-level logic of error handling and
       result presentation.
    
    3. Accurate Root Path Accounting: The `dfs` function assesses the disk
       usage of the `rootpath` itself, ensuring this value is included whether
       `rootpath` is a file or a directory.

--- du.c
+++ du.c
@@ -13,7 +13,8 @@ extern int optind;
 static const int kMaxArgs = 3;
 static int include_files = 0;
 
-uint64_t du(const char *rootpath);
+int du(const char *rootpath);
+uint64_t dfs(const char *rootpath, int *error);
 void PrintUsage(const char *cmd);
 void PrintDiskUsage(uint64_t disk_usage, const char *path);
 
@@ -46,22 +47,49 @@ int main(int argc, char *argv[]) {
     }
 
     const char *pathname = (optind < argc) ? argv[optind] : ".";
-    if (du(pathname) == (uint64_t)-1) {
+    if (du(pathname) < 0) {
         return EXIT_FAILURE;
     }
     
     return EXIT_SUCCESS;
 }
 
-uint64_t du(const char *rootpath) {
-    // TODO: Program should be able to take regular file paths as input
-    struct stat statbuf;
+int du(const char *rootpath) {
+    int error = 0;
+    dfs(rootpath, &error);
+
+    if (error) {
+        fprintf(stderr, "%s\n", strerror(error));
+        return -1;
+    }
+
+    return 0;
+}
+
+uint64_t dfs(const char *rootpath, int *error) {
+    struct stat statbuf;    
+    uint64_t disk_usage_kb;
     uint64_t total = 0;
+
+    if (lstat(rootpath, &statbuf) < 0) {
+        *error = errno;
+        return 0;
+    }
+
+    disk_usage_kb = statbuf.st_blocks / 2;
+    
+    // du was called on a regular file
+    if (!S_ISDIR(statbuf.st_mode)) {
+        PrintDiskUsage(disk_usage_kb, rootpath);
+        return disk_usage_kb;
+    } else {
+        total += disk_usage_kb;
+    }
     
     DIR *dirp = opendir(rootpath);
     if (!dirp) {
-        fprintf(stderr, "Error: Could not open directory '%s': %s\n", rootpath, strerror(errno));
-        return (uint64_t)-1;
+        *error = errno;
+        return 0;
     }
 
     struct dirent *direntp;
@@ -71,47 +99,33 @@ uint64_t du(const char *rootpath) {
             continue;
         }
 
-        // Append file name to root path 
         char pathname[PATH_MAX];
         if (snprintf(pathname, PATH_MAX, "%s/%s", rootpath, direntp->d_name) < 0) {
-            fprintf(stderr, "Error: Failed to generate pathname for '%s/%s'\n", rootpath, direntp->d_name);
-            closedir(dirp);
-            return (uint64_t)-1;
+            *error = errno;
+            break;
         }
 
         if (lstat(pathname, &statbuf) < 0) {
-            fprintf(stderr, "Error: Failed to get stat for '%s': %s\n", pathname, strerror(errno));
-            closedir(dirp);
-            return (uint64_t)-1;
+            *error = errno;
+            break;
         }
 
-        uint64_t disk_usage_kb = statbuf.st_blocks / 2;
-        switch (statbuf.st_mode & S_IFMT) {
-            case S_IFDIR: {
-                total += du(pathname);
+        disk_usage_kb = statbuf.st_blocks / 2;
+        if (S_ISDIR(statbuf.st_mode)) {
+            total += dfs(pathname, error);
+            if (*error) {
                 break;
             }
-            case S_IFLNK:
-            case S_IFREG: {
-                total += disk_usage_kb;
-                if (include_files) {
-                    PrintDiskUsage(disk_usage_kb, pathname);
-                }
-                continue;
+        } else if (S_ISLNK(statbuf.st_mode) || S_ISREG(statbuf.st_mode)) {
+            total += disk_usage_kb;
+            if (include_files) {
+                PrintDiskUsage(disk_usage_kb, pathname);
             }
-            default:
-                continue;
         }
     }
     closedir(dirp);
-
-    if (lstat(rootpath, &statbuf) < 0) {
-        fprintf(stderr, "Failed to get stat on '%s': %s\n", rootpath, strerror(errno));
-        return (uint64_t)-1;
-    }
-    total += statbuf.st_blocks / 2;
+    
     PrintDiskUsage(total, rootpath);
-
     return total;
 }
 
